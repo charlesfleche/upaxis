44610bf47a3f710169980714f7eae3d08d80158f
Charles Flèche <charles.fleche@free.fr>
2024-03-21 18:07:48 -0400

    WIP scratch

--- scratch.py
+++ scratch.py
@@ -1,53 +1,57 @@
-from pxr import Gf
+from pxr import Gf, Usd, UsdGeom
 
-o = Gf.Vec3d(0, 0, 0)
+s = Usd.Stage.Open("test-compensation_skeleton.usda")
 
-x1 = Gf.Vec3d(1, 0, 0)
-y1 = Gf.Vec3d(0, 1, 0)
-z1 = Gf.Vec3d(0, 0, 1)
+p = s.GetPrimAtPath("/SkelRoot_CompensatedZToYDecimetersToMeters")
+c = UsdGeom.XformCache()
+print(c.GetLocalToWorldTransform(p))
+print(c.GetParentToWorldTransform(p))
+m, _ = c.GetLocalTransformation(p)
+print(m)
 
-Ryz = Gf.Rotation(x1, 90)
-Tyz = Gf.Matrix4d().SetIdentity().SetRotate(Ryz)
-Tzy = Tyz.GetInverse()
+print()
 
-print("Tyz", Tyz)
-for v in (x1, y1, z1):
-    print(v)
-    print(Tyz.Transform(v))
+p = s.GetPrimAtPath("/SkelRoot_CompensatedZToYDecimetersToMeters/Skeleton")
+c = UsdGeom.XformCache()
+print(c.GetLocalToWorldTransform(p))
+print(c.GetParentToWorldTransform(p))
+m, _ = c.GetLocalTransformation(p)
+print(m)
 
-    mv = Gf.Matrix4d().SetIdentity().SetTranslate(v)
-    print((mv * Tyz).Transform(o))
-    print()
 
-print("#\n")
+L = c.GetLocalToWorldTransform(p)
 
-p0 = Gf.Vec3d(0, 0, 1)
-p1 = Gf.Vec3d(.5, 0, 1)
-p2 = Gf.Vec3d(0, 0, 2)
+print()
 
-print("Tzy", Tzy)
-for v in (p0, p1, p2):
-    print(v)
-    print(Tzy.Transform(v))
-    print()
+O = Gf.Vec3d(0, 0, 0)
 
-print("#\n")
+r = Gf.Rotation(Gf.Vec3d(0, 1, 0), -90)
+t = Gf.Vec3d( 0, 0, 1)
 
-bb = Gf.Matrix4d(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-	0.0, 0.0, 1.0, 0.0,
-	0.0, 0.0, 1.0, 1.0
-)
+p0 = Gf.Vec3d( 0, 0, 0)
+p1 = Gf.Vec3d(.5, 0, 0)
+p2 = Gf.Vec3d( 0, 0, 1)
 
-bt = Gf.Matrix4d(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-	0.0, 0.0, 1.0, 0.0,
-	0.0, 0.0, 2.0, 1.0
-)
+GBT = Gf.Transform()
+GBT.SetRotation(r)
+GBT.SetTranslation(t)
 
-for m in (bb, bt):
-    print(m)
-    print(m * Tzy)
-    print(m * Tzy * Tyz)
+print("GBT ->", GBT.GetMatrix())
+print()
+
+for p in (p0, p1, p2):
+    print(p, "->", GBT.GetMatrix().Transform(p))
+
+print()
+
+b0 = Gf.Transform()
+b0.SetTranslation(p0)
+b0 = b0.GetMatrix()
+
+b1 = Gf.Transform()
+b1.SetTranslation(p2)
+b1 = b1.GetMatrix()
+
+for bt in (b0, b1):
+    m = bt * GBT.GetMatrix()
+    print(bt, "->", m, "->", m.Transform(O))
